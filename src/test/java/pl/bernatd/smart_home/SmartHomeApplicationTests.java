014610fa82ba35d12bb8729dc6e387e65457f259
Damian Bernat <damianbernat@wp.pl>
2022-10-26 18:56:48 +0200

    Initial commit

---
+++ src/test/java/pl/bernatd/smart_home/SmartHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bernatd.smart_home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
